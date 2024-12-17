aaf6f727ee471104c5a267bbc15a8b002c426d0a
aditya8161 <adityasutar18092003@gmail.com>
2024-12-17 16:03:10 +0530

    add coursespringjdbc1 file

---
+++ CourseSpringJdbc1/CourseSpringJdbc1/src/main/java/com/course/example/CourseSpringJdbc1/CourseSpringJdbc1Application.java
@@ -0,0 +1,13 @@
+package com.course.example.CourseSpringJdbc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringJdbc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringJdbc1Application.class, args);
+	}
+
+}
---
+++ CourseSpringJdbc1/CourseSpringJdbc1/src/test/java/com/course/example/CourseSpringJdbc1/CourseSpringJdbc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.example.CourseSpringJdbc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringJdbc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
