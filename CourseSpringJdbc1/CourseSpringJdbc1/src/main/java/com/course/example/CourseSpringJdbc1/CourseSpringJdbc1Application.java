fef4bb8041c8018d5b4fae2f2dca8d6f8e0d9048
aditya8161 <adityasutar18092003@gmail.com>
2024-12-17 16:09:29 +0530

    Change in main class

--- CourseSpringJdbc1/CourseSpringJdbc1/src/main/java/com/course/example/CourseSpringJdbc1/CourseSpringJdbc1Application.java
+++ CourseSpringJdbc1/CourseSpringJdbc1/src/main/java/com/course/example/CourseSpringJdbc1/CourseSpringJdbc1Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseSpringJdbc1Application {
+public class CourseSpringJdbc1Application
+{
 
 	public static void main(String[] args)
 	{
